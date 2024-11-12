fd33d08a376f7a59d55a5cb50407cb022823e3c3
klaudiagozdz <gozdzk05@gmail.com>
2024-11-12 14:19:47 +0000

    sprawdzenie4

--- python1.py
+++ python1.py
@@ -11,7 +11,7 @@ def oblicz_sume_liczb_parzystych(lista):
         
 print(oblicz_sume_liczb_parzystych(3,2,5,7,6)) # 8
 
-
+#2
 # Stwórz funkcję, która znajdzie liczbę nieparzystych elementów w liście 
 # przy użyciu pętli for i operatora warunkowego if.
 def liczba_nieparzystych_elementow(lista):
@@ -22,7 +22,7 @@ def liczba_nieparzystych_elementow(lista):
     return liczba_elementow
 
         
-
+#3
 # Utwórz funkcję, która znajdzie drugi największy element w liście 
 # za pomocą pętli for i operatora warunkowego if
 def drugi_najwiekszy_element(lista):
@@ -33,7 +33,7 @@ def drugi_najwiekszy_element(lista):
     posortowana_lista = sorted(lista)
     return posortowana_lista[-2]
 
-
+#4
 #Zaimplementuj funkcję, która sprawdzi, czy w liście znajduje się co najmniej jeden element ujemny, 
 # korzystając z pętli for i operatora warunkowego if.
 def minimum_jeden_ujemny_element(lista):
@@ -42,7 +42,7 @@ def minimum_jeden_ujemny_element(lista):
             return True
         return False
 
-
+#5
 # Napisz funkcję, która znajdzie liczbę dodatnich liczb w liście 
 # przy użyciu pętli for i operatora warunkowego if.
 def liczba_dodatnich_elementow(lista):
@@ -52,7 +52,7 @@ def liczba_dodatnich_elementow(lista):
             liczba_elementow +=1
     return liczba_elementow
 
-
+#6
 # Napisz funkcję, która obliczy średnią temperaturę z listy temperatur, 
 # za pomocą pętli for i zwróci wynik zaokrąglony do dwóch miejsc po przecinku.
 def oblicz_srednia_temperature(lista):
@@ -64,7 +64,7 @@ def oblicz_srednia_temperature(lista):
         srednia = suma/len(lista)
         return round(srednia, 2)
 
-    
+#7    
 # Napisz funkcję, która obliczy średnią ocen z listy, 
 # za pomocą pętli for i zwróci wynik zaokrąglony do dwóch miejsc po przecinku.
 def oblicz_srednia_ocen(lista_ocen):
@@ -76,7 +76,7 @@ def oblicz_srednia_ocen(lista_ocen):
         return round(srednia_ocen, 2)
 
 
-
+#8
 # Napisz funkcję, która obliczy średnią długość słów w liście, 
 # za pomocą pętli for i zwróci wynik zaokrąglony do dwóch miejsc po przecinku.
 def oblicz_dlugosc_slow(lista_slow):
@@ -95,7 +95,7 @@ def oblicz_dlugosc_slow(lista_slow):
 
 
 
-
+#9
 # importuj biblioteke statistics i za jej pomoca zaimplementuj 
 # funkcje srednia_import
 # wynik zaokrąglij do 2 miejsca po przecinku
@@ -110,7 +110,7 @@ def oblicz_srednia_import(lista):
 
 
 
-
+#10
 # Napisz funkcję, która obliczy ocenę studenta na podstawie listy wyników egzaminów, przy czym ocena będzie wynosiła 2, 
 # jeśli przynajmniej połowa wyników jest niższa od 50 punktów, 
 # w przeciwnym razie oblicz średnią z wyników i przypisz ją jako ocenę.
@@ -125,7 +125,7 @@ def oblicz_ocene(lista):
 
 
     
-
+#11
 # Napisz funkcję, która obliczy ocenę studenta na podstawie listy liczby punktów zdobytych w różnych przedmiotach. 
 # Jeśli przynajmniej połowa przedmiotów ma wynik większy niż 60 punktów, 
 # oblicz średnią z tych przedmiotów i przypisz ją jako ocenę. Jeśli nie, przypisz ocenę 2.
@@ -138,6 +138,7 @@ def oblicz_ocene(lista):
         return 2
 # zamiast pozytywne wyniki lepiej uzyc ppzytywne przedmioty
 
+#12
 # Napisz funkcję, która obliczy ocenę studenta na podstawie wyników z różnych testów. 
 # Jeśli przynajmniej połowa testów została zdana (wynik > 60), 
 # to ocena to średnia wyników z tych testów. Jeśli nie, ocena wynosi 2.
@@ -150,7 +151,7 @@ def oblicz_ocene(lista):
         return 2
 # zamiast pozytywne wyniki można użyć pozytywne testy
 
-
+#13
 # Napisz funkcję, która obliczy średnią ważoną ocen studenta, gdzie każda ocena jest reprezentowana przez listę zawierającą 
 # dwa elementy: 
 # wagę oceny i samą ocenę. Wynik funkcji ma być zaokrąglony do dwóch miejsc po przecinku.
@@ -162,41 +163,111 @@ def oblicz_srednia_wazona(lista):
     srednia_wazona = suma_wazonych_ocen / suma_wag
     return round(srednia_wazona, 2)
 
-#Zadanie 1: Funkcja generująca skrót z pierwszych liter każdego słowa Napisz funkcję, 
+#Zadanie 14: Funkcja generująca skrót z pierwszych liter każdego słowa Napisz funkcję, 
 # która przyjmuje zdanie jako argument i zwraca skrót utworzony z pierwszych liter każdego słowa, zapisany dużymi literami.
 # Wynik ma być zwrócony jako jeden ciąg znaków (bez kropek między literami).
+def pierwsze_litery_slowa(slowa):
+    slowa_lista = slowa.split()
+    inicjaly = ""
+    for element in slowa_lista:
+         inicjaly += element[0].upper()
+    return inicjaly
 
-#Zadanie 2: Funkcja do konwersji pełnego imienia na nazwisko i inicjał imienia Napisz funkcję, 
+#Zadanie 15: Funkcja do konwersji pełnego imienia na nazwisko i inicjał imienia Napisz funkcję, 
 # która przyjmuje pełne imię i nazwisko jako argument i zwraca je w formacie: N. Nazwisko, 
 # gdzie N to inicjał imienia zapisany wielką literą. Całe nazwisko powinno być również zapisane dużymi literami.
+def konwertuj_imie_nazwisko(imie_nazwisko):
+    imie_nazwisko_lista = imie_nazwisko.split()
+    if len(imie_nazwisko_lista) < 2:
+        return "Błędne dane"
+    inicjal = imie_nazwisko_lista[0][0].upper = "."  #imie_nazwisko_lista[0] to pierwsze słowo w tej liście (czyli imię).
+                                                    #imie_nazwisko_lista[0][0]: Pobiera pierwszy znak pierwszego słowa (czyli pierwszą literę imienia).
+    nazwisko = imie_nazwisko_lista[-1].upper()  #imie_nazwisko_lista[-1]: Zwraca ostatni element listy imie_nazwisko_lista, którym jest nazwisko (w przypadku, 
+                                                #gdy lista zawiera tylko imię i nazwisko, to będzie to nazwisko).
+                                                #.upper(): Zamienia całe nazwisko na wielkie litery
+    return inicjal +" "+ nazwisko 
 
-#Zadanie 1: Zwrot unikalnych znaków w kolejności wystąpienia Napisz funkcję, 
+
+
+
+#Zadanie 16: Zwrot unikalnych znaków w kolejności wystąpienia Napisz funkcję, 
 # która przyjmuje ciąg znaków i zwraca listę unikalnych znaków w kolejności ich pierwszego pojawienia się.
 #Przykład:
 #Wejście: "abracadabra"
 #Oczekiwane wyjście: ['a', 'b', 'r', 'c', 'd']
+def zwroc_liste_unikalnych_znakow(ciag):
+    napotkane = {}
+    for element in ciag:
+        if element not in ciag:
+            napotkane.append(element) #Użycie add() jest poprawne dla zbiorów (set), ale nie dla list. musi  być append
+    return napotkane
+          
 
-#Zadanie 2: Znajdowanie najczęściej występującego znaku w ciągu Napisz funkcję, 
+
+#Zadanie 17: Znajdowanie najczęściej występującego znaku w ciągu Napisz funkcję, 
 # która przyjmuje ciąg znaków i zwraca najczęściej występujący znak oraz liczbę jego wystąpień.
 #Przykład:
 #Wejście: "banana"
 #Oczekiwane wyjście: ('a', 3)
+def zwroc_najczesciej_wystepujacy_znak(slowo):
+    licznik = {}
+    for element in slowo:
+        if element in slowo:
+            licznik[element] += 1
+        else:
+            licznik[element] = 1
+            najczestszy_znak = max(licznik, key=licznik.get)
+    return najczestszy_znak, licznik[najczestszy_znak]
 
-#Zadanie 3: Liczenie samogłosek i spółgłosek w ciągu Napisz funkcję, 
+#max() to funkcja, która zwraca największy element w podanym iterowalnym obiekcie (w tym przypadku klucz w słowniku licznik).
+#key=licznik.get: Argument key pozwala określić, według jakiej zasady max() wybiera największy element. 
+#licznik.get jest funkcją, która dla danego klucza zwraca jego wartość (liczbę wystąpień znaku).
+#Działanie: max() przegląda wszystkie klucze w licznik i zwraca ten klucz, dla którego licznik.get(klucz) 
+# (czyli liczba wystąpień) jest największa.
+
+
+
+#Zadanie 18: Liczenie samogłosek i spółgłosek w ciągu Napisz funkcję, 
 # która przyjmuje ciąg znaków i zwraca liczbę samogłosek i spółgłosek.
 #Przykład:
 #Wejście: "hello"
 #Oczekiwane wyjście: {'samogloski': 2, 'spolgloski': 3}
+def zwoc_liczbe_samoglosek_i_spolglosek(slowo):
+    licznik = {"samogloski": 0, "spolgloski": 0}
+    samogloski = "aeouiyAEOUIY"
+    spolgloski = "bBcCDdfFgGHhJjKklLZzVvNnMmWwRrTtPp"
+    for element in slowo:
+        if element in samogloski:
+            licznik["samogloski"] +=1
+        elif element in spolgloski:
+            licznik["spolgloski"] +=1
+    return licznik
 
-#Zadanie 4: Sprawdzenie anagramów Napisz funkcję, która przyjmuje dwa ciągi znaków i sprawdza, 
+
+
+#Zadanie 19: Sprawdzenie anagramów Napisz funkcję, która przyjmuje dwa ciągi znaków i sprawdza, 
 # czy są one anagramami (tzn. czy składają się z tych samych liter w różnych kolejnościach).
 #Przykład:
 #Wejście: "listen", "silent"
 #Oczekiwane wyjście: True
+def czy_anagramy(ciag1, ciag2):
+    return sorted(ciag1) == sorted(ciag2)  #Jeśli dwa ciągi mają te same znaki w tej samej liczbie, to po posortowaniu będą wyglądały identycznie
 
 
-#Zadanie 5: Liczenie słów w ciągu znaków Napisz funkcję, 
+
+
+
+#Zadanie 20: Liczenie słów w ciągu znaków Napisz funkcję, 
 # która przyjmuje ciąg znaków i zwraca słownik z liczbą wystąpień każdego słowa w tym ciągu.
 #Przykład:
 #Wejście: "to be or not to be"
 #Oczekiwane wyjście: {'to': 2, 'be': 2, 'or': 1, 'not': 1}
+def oblicz_slowa_zdanie(zdanie):
+    posortowana_zdanie = zdanie.split()
+    licznik = {}
+    for element in posortowana_zdanie:
+        if element in licznik: # nie posortowana_zdanie
+            licznik[element] += 1
+        else:
+            licznik[element] = 1 #Jeśli usuniesz else, funkcja nie będzie wiedziała, co zrobić, gdy napotka nowe słowo
+    return licznik
